d6adc38d1d59d995c17cd227757cad65fa84fc39
maria <uo287585@uniovi.es>
2025-03-24 16:03:49 +0100

    logica registro (casi acabada)

--- TravelTales/src/Components/RegisterUserComponent.jsx
+++ TravelTales/src/Components/RegisterUserComponent.jsx
@@ -3,73 +3,102 @@ import {backendUrl} from "../Globals"
 import { useNavigate } from "react-router-dom";
 
 let RegisterUserComponent = () =>{
-    let [name,setName] =useState(null)
-    let [password,setPassword] =useState("")
+    let [nombre,setNombre] =useState(null)
+    let [apellidos,setApellidos] =useState(null)
     let [email,setEmail] =useState(null)
-
-    let [message,setMessage]=useState("")
+    let [contrasena,setContrasena] =useState(null)
+    let [contrasena2,setContrasena2] =useState(null)
+    
+    let [mensaje,setMensaje]=useState("")
     let [error,setError]=useState({})
     let navigate = useNavigate()
     useEffect(()=>{
         checkData();
-    },[name,email,password])
+    },[nombre,apellidos,email,contrasena,contrasena2])
 
     let checkData = () =>{
-        let newErrors = {}
-        if( name == "" )
-            newErrors.name= "Name must have a value"
+        let errores = {}
+        if( nombre == "" )
+            errores.nombre= "El campo 'nombre' debe tener un valor"
+        if( apellidos == "" )
+            errores.apellidos= "El campo 'apellidos' debe tener un valor"
         if(email == "" )
-            newErrors.email= "Email must have a value"
-        if(password == "" )
-            newErrors.password= "Password must have a value"
-        setError(newErrors)
+            errores.email= "El campo 'email' debe tener un valor"
+        if(contrasena == "" )
+            errores.contrasena= "El campo 'contraseña' debe tener un valor"
+        if(contrasena2 == "" )
+            errores.contrasena2= "El campo 'repetir contraseña' debe tener un valor"
+        if(contrasena!==contrasena )
+            errores.coincicir= "Las dos contraseñas deben coindicir"
+        setError(errores)
     }
     let registerUser = async() =>{
+        //añadir logica para que si errores, no deje hacer la peticion
         let response = await fetch(backendUrl+"/users",
         {method: "POST",
             headers: {"Content-Type":"application/json"},
             body: JSON.stringify({
+                nombre:nombre,
+                apellidos:apellidos,
                 email:email,
-                name:name,
-                password:password
+                contrasena:contrasena
             }) 
         })
         if(response.ok){
-            //let jsonData= await response.json()
             navigate("/login")
         }else{
             let jsonData = await response.json()
-            let errors=""
+            let errores=""
             if(jsonData.errors!=null){
                 jsonData.errors.array.forEach(e => {
-                    errors+=e+" "
+                    errores+=e+" "
                 });
-                setMessage(errors)
+                setMensaje(errores)
             }else
-                setMessage(jsonData.error)
+                setMensaje(jsonData.error)
             
         }
     }
 
     return (
-        <div>
-            <h2>Register User</h2>
-            <h3>{message}</h3>
-            <div>
-                <div>
-                    <input type='text' placeholder='email' onChange={(e)=>setEmail(e.currentTarget.value)}/>
-                </div>
-                {error.email && <p>{error.email}</p>}
-                <div>
-                    <input type='text' placeholder='name' onChange={(e)=>setName(e.currentTarget.value)}/>
-                </div>
-                {error.name && <p>{error.name}</p>}
-                <div>
-                    <input type='text' placeholder='password' onChange={(e)=>setPassword(e.currentTarget.value)}/>
-                </div>
-                {error.password && <p>{error.password}</p>}
+        <div class='card'>
+            <div class='card-body'>
+                <h2 class='card-title'>Registrarse</h2>
+                <h3>{mensaje}</h3>
+                <form>
+                    <div class='mb-3'>
+                        <label for="nombre" class='form-label'>NOMBRE</label>
+                        <input id='nombre' class='form-control' type='text' placeholder='Nombre' onChange={(e)=>setNombre(e.currentTarget.value)}/>
+                        {error.nombre && <p class='text-danger'>{error.nombre}</p>}
+                    </div>
+                    <div class='mb-3'>
+                        <label for="apellidos" class='form-label'>APELLIDOS</label>
+                        <input id='apellidos' class='form-control' type='text' placeholder='Apellidos' onChange={(e)=>setApellidos(e.currentTarget.value)}/>
+                        {error.apellidos && <p class='text-danger'>{error.apellidos}</p>}
+                    </div>
+                    
+                    <div class='mb-3'>
+                        <label for="correo" class='form-label'>CORREO</label>
+                        <input id='correo' class='form-control' type='text' placeholder='correo@correo.com' onChange={(e)=>setEmail(e.currentTarget.value)}/>
+                        {error.email && <p class='text-danger'>{error.email}</p>}
+                    </div>
+                    
+                    <div class='mb-3'>
+                        <label for="contrasena" class='form-label'>CONTRASEÑA</label>
+                        <input id='contrasena' class='form-control' type='password' onChange={(e)=>setContrasena(e.currentTarget.value)}/>
+                        {error.contrasena && <p class='text-danger'>{error.contrasena}</p>}
+                    </div>
+                    
+                    <div class='mb-3'>
+                        <label for="contrasena2" class='form-label'>REPETIR CONTRASEÑA</label>
+                        <input id='contrasena2' class='form-control' type='password' onChange={(e)=>setContrasena2(e.currentTarget.value)}/>
+                        {error.contrasena2 && <p class='text-danger'>{error.contrasena2}</p>}
+                        {error.coincidir && <p class='text-danger'>{error.coincidir}</p>}
+                    </div>
+                    
+                    <button class='btn btn-primary' onClick={registerUser}>Crear Cuenta</button>
+                </form>
             </div>
-            <button onClick={registerUser}>Register</button>
         </div>
     )
 }
