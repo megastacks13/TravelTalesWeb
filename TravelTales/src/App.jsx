a563e59cc371ae6c16cd8114fdbd2e7bab943eb1
David Xu <davidxu@ucm.es>
2025-03-26 16:15:16 +0100

    La navbar mas o menos terminada

--- TravelTales/src/App.jsx
+++ TravelTales/src/App.jsx
@@ -46,7 +46,7 @@ function App() {
     
     <div className="App">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
-      {login && <NavBarComponent />}
+      {login && <NavBarComponent disconnect={disconnect}/>}
   
       {notification!="" && (
         <div className='notification'>      {notification}
--- TravelTales/src/Components/NavBarComponent.jsx
+++ TravelTales/src/Components/NavBarComponent.jsx
@@ -5,9 +5,7 @@ import FormField from "./FormFieldComponent.jsx";
 import '../NavBar.css';
 
 let NavBarComponent = (props) =>{
-
-
-    
+    let {disconnect}=props
     return (
         <div>
             <div class="container">
@@ -16,20 +14,24 @@ let NavBarComponent = (props) =>{
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
+                <div class="vr bg-light"></div>
                 <div class="collapse navbar-collapse " id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
+                    <ul class="navbar-nav ms-auto">
                         <li class="nav-item active">
                             <a class="nav-link" href="/register">Añadir Viajes</a>
                         </li>
+                        <div class="vr bg-light"></div>
                         <li class="nav-item">
                             <a class="nav-link" href="/register">Buscar Viajes</a>
                         </li>
-                        <li class="nav-item ml-auto">
-                            <a class="nav-link" href="/">Cerrar sesión</a> 
-                        </li>
                     </ul>
                     
+                    <ul class="navbar-nav ms-auto"> 
+                        <div class="vr bg-light"></div>
+                        <li class="nav-item cerrar-sesion">
+                            <a class="nav-link link-cerrar-sesion" href="/" onClick={disconnect}>Cerrar sesión</a> 
+                        </li>
+                    </ul>  
                 </div>
             </nav>
             </div>
