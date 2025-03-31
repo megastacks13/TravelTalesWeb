46141fc43cb2a476a3c5bc6364d9f6d542053888
maria <uo287585@uniovi.es>
2025-03-31 12:33:26 +0200

    correccion front

--- TravelTales/backend/routers/routerViajes.js
+++ TravelTales/backend/routers/routerViajes.js
@@ -4,7 +4,7 @@ import database from "../database.js";
 const { db, usersRef, viajesRef } = database;
 
 routerViajes.post("/anadir", async (req, res) => {
-    const { nombre, ubicacion, fechaIni, fechaFin, num, correoUser } = req.body;
+    const { nombre, ubicacion, fechaIni, fechaFin, num } = req.body;
     let errors = [];
     if (!db) errors.push('Database error')
     if (!nombre) errors.push("No se ha recibido un nombre");
@@ -12,29 +12,31 @@ routerViajes.post("/anadir", async (req, res) => {
     if (!fechaIni) errors.push("No se han recibido una fecha de inicio");
     if (!fechaFin) errors.push("No se han recibido una fecha de finalización");
     if (!num) errors.push("No se ha recibido un número de personas");
-    if (!correoUser) errors.push("No se ha recibido el correo del usuario");
+    let email=req.infoApiKey.email
+    if (!email) errors.push("No se ha recibido el correo del usuario");
 
-    const fechaRegex =/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(\d{4})$/
+    const fechaRegex = /^\d{4}-\d{2}-\d{2}$/;
 
-    if (fechaIni && !fechaRegex.test(fechaIni)) errors.push("La fecha de inicio no tiene un formato válido (dd/mm/yyyy) o contiene valores incorrectos.");
-    if (fechaFin && !fechaRegex.test(fechaFin)) errors.push("La fecha de finalización no tiene un formato válido (dd/mm/yyyy) o contiene valores incorrectos.");
-    
+    if (fechaIni && !fechaRegex.test(fechaIni)) errors.push("La fecha de inicio no tiene un formato válido (yyyy-mm-dd) o contiene valores incorrectos.");
+    if (fechaFin && !fechaRegex.test(fechaFin)) errors.push("La fecha de finalización no tiene un formato válido (yyyy-mm-dd) o contiene valores incorrectos.");
+
     const parseFecha = (fecha) => {
-        const [dia, mes, año] = fecha.split('/').map(Number);
-
+        const [año, mes, dia] = fecha.split('-').map(Number);
+        
         const fechaDate = new Date(año, mes - 1, dia);
-        if (fechaDate.getDate() !== dia || fechaDate.getMonth() !== mes - 1) {
+
+        if (fechaDate.getFullYear() !== año || fechaDate.getMonth() !== mes - 1 || fechaDate.getDate() !== dia) {
             throw new Error("La fecha no es válida, el día no corresponde al mes.");
         }
-    
+
         return fechaDate;
     };
-    
+
     if (fechaIni && fechaFin && fechaRegex.test(fechaIni) && fechaRegex.test(fechaFin)) {
         try {
             const fechaInicioDate = parseFecha(fechaIni);
             const fechaFinDate = parseFecha(fechaFin);
-    
+
             if (fechaFinDate <= fechaInicioDate) {
                 errors.push("La fecha de finalización debe ser posterior a la fecha de inicio");
             }
@@ -54,14 +56,16 @@ routerViajes.post("/anadir", async (req, res) => {
         errors.push("La ubicación debe comenzar con una letra, tener al menos 3 caracteres y solo contener letras, números y caracteres especiales (.,/ -).");
     }
 
+    console.log(errors)
     if (errors.length > 0) return res.status(400).json({ errors });
 
+
     try {
-        const snapshot2 = await usersRef.orderByChild("email").equalTo(correoUser).once("value");
+        const snapshot2 = await usersRef.orderByChild("email").equalTo(email).once("value");
         if (!snapshot2.exists()) {
             return res.status(401).json({ error: "No existe un usuario con ese correo" });
         }else{
-            const snapshot = await viajesRef.orderByChild("correoUser").equalTo(correoUser).once("value");
+            const snapshot = await viajesRef.orderByChild("correoUser").equalTo(email).once("value");
             if (snapshot.exists()) {
                 const viajes = snapshot.val();
                 const viajeConMismoNombre = Object.values(viajes).find(viaje => viaje.nombre === nombre);
@@ -73,9 +77,9 @@ routerViajes.post("/anadir", async (req, res) => {
         }
 
         const newViajeRef = viajesRef.push();
-        await newViajeRef.set({ nombre, ubicacion, fechaIni, fechaFin, num, correoUser });
+        await newViajeRef.set({ nombre, ubicacion, fechaIni, fechaFin, num, email });
 
-        res.json({ viajeAnadido: { id: newViajeRef.key, nombre, ubicacion, fechaIni, fechaFin, num, correoUser } });
+        res.json({ viajeAnadido: { id: newViajeRef.key, nombre, ubicacion, fechaIni, fechaFin, num, email } });
     } catch {
         res.status(402).json({ error: "Ha habido un error insertando el viaje" });
     }
