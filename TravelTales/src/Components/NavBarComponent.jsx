bde300786e77ef2fe2ea568212b256022cf7c8ad
David Xu <davidxu@ucm.es>
2025-04-01 18:21:30 +0200

    arreglados detalles de la navbar y la lista de viajes

--- TravelTales/src/Components/NavBarComponent.jsx
+++ TravelTales/src/Components/NavBarComponent.jsx
@@ -1,12 +1,14 @@
 import '../NavBar.css';
+import {useNavigate} from 'react-router-dom'
 
 let NavBarComponent = (props) =>{
-    let {disconnect}=props
+    let {disconnect}=props;
+    let navigate = useNavigate();
     return (
         <div>
             <div class="container">
             <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" href="/">Travel Tales</a>
+                <a class="navbar-brand" onClick={() => navigate("/inicio")}>Travel Tales</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -14,11 +16,11 @@ let NavBarComponent = (props) =>{
                 <div class="collapse navbar-collapse " id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="/viajes/anadir">Añadir Viajes</a>
+                            <a class="nav-link" onClick={() => navigate("/viajes/anadir")}>Añadir Viajes</a>
                         </li>
                         <div class="vr bg-light"></div>
                         <li class="nav-item">
-                            <a class="nav-link" href="/viajes/buscar">Buscar Viajes</a>
+                            <a class="nav-link" onClick={() => navigate("/viajes/buscar")}>Buscar Viajes</a>
                         </li>
                     </ul>
                     
