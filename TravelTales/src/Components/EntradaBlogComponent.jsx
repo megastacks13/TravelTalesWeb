aa64ccc91c884fa6216096680f1ba43939e71414
ruben <rubenvarj@gmail.com>
2025-04-24 15:59:50 +0200

    continuando co la iu

--- TravelTales/src/Components/EntradaBlogComponent.jsx
+++ TravelTales/src/Components/EntradaBlogComponent.jsx
@@ -1,16 +1,20 @@
 import { useState,useEffect } from "react";
 import { backendUrl } from "../Globals.js";
-import { useNavigate } from "react-router-dom";
+import { Form, useNavigate } from "react-router-dom";
 import FormField from "./FormFieldComponent.jsx";
 
-let EntradaBlogComponent = ( props)=>{    
+let EntradaBlogComponent = ( props)=>{  
+    let [titulo,setTitulo] =useState(null)  
+    let [fecha,setFecha]=useState(null)
+    let [texto,setTexto]=useState(null)
+
     let {createNotification}=props
     let [mensaje,setMensaje]=useState("")
     let [error,setError]=useState({})
     let navigate = useNavigate()
     useEffect(()=>{
         checkData();
-    },[nombre,ubicacion,fechaIni,fechaFin,numero])
+    },[titulo,fecha,texto])
     
 
     let addEntrada = async(event) => {
@@ -48,14 +52,32 @@ let EntradaBlogComponent = ( props)=>{
     }
 
     return (
-    <div class='card bg-transparent'>
+    <div class='card bg-transparent'> 
         <h1 class="card-header">Travel Tales</h1>
         
         <div class='card-body bg-white  carta-registro'>
             <h2 class='card-title'>Añadir viaje</h2>
             <h3 class="errorMessage">{mensaje}</h3>
             <form>
-
+                <FormField 
+                    id="titulo" 
+                    label="TITULO" 
+                    placeholder="Titulo" 
+                    value={titulo} onChange={(e) => setTitulo(e.currentTarget.value)} 
+                    errors={error.nombre ? [error.nombre] : []} />
+                <FormField 
+                    id="fecha" 
+                    label="FECHA" 
+                    placeholder="Fecha" 
+                    value={fecha} onChange={(e) => setFecha(e.currentTarget.value)} 
+                    errors={error.nombre ? [error.nombre] : []} />
+                <FormField
+                    id="texto"
+                    label="TEXTO"
+                    placeholder="Texto"
+                    value={texto} onChange={(e) => setTexto(e.currentTarget.value)}
+                    errors={error.nombre ? [error.nombre] : []}
+                />
                 <div className='d-flex justify-content-between mt-3'> 
                     <button class='btn btn-sm btn-secondary me-2' type='button' onClick={() => window.history.back()}>Volver Atrás</button>
                     <button class='btn btn-sm btn-primary' type='submit' onClick={addTravel}>Añadir viaje</button>
