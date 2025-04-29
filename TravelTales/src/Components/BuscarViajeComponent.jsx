20971ef6cab46a5da8149d6e6569f8c3a04f07ee
David Xu <davidxu@ucm.es>
2025-04-29 13:03:46 +0200

    revision buscar viaje, algunos cambios pequeños

--- TravelTales/src/Components/BuscarViajeComponent.jsx
+++ TravelTales/src/Components/BuscarViajeComponent.jsx
@@ -15,11 +15,11 @@ let BuscarViajeComponent = ( )=>{
     let navigate = useNavigate()
 
     function isNameEmpty(name) {
-        return name===null || name === undefined || name.trim() === "";
+        return name === null || name === undefined || name.trim() === "";
     }
   
     let buscarViaje = async() =>{
-        if (isNameEmpty(nombre)){
+        if (isNameEmpty(nombre)){ // si no la búsqueda es vacía
             let response = await fetch(backendUrl + "/viajes?apiKey=" + localStorage.getItem("apiKey"))
             if (response.status == 401) {
                 navigate("/login")
@@ -45,23 +45,18 @@ let BuscarViajeComponent = ( )=>{
 
     return (
     <div>
-        
         <div class='card bg-transparent container-misViajes'>
-            <h1>Buscar por localización</h1>
-            
-            <div class='card-body bg-white buscar-viaje'>
+            <h1 class='mt-4'>Buscar viaje por localización</h1>
+            <div class='card-body bg-white buscar-viaje w-50'>
                 <form>
                     <FormField id="nombre" label="Ubicación" placeholder="Paris, Roma ..." value={nombre} onChange={(e) => setNombre(e.currentTarget.value)} />
-
                     <div className='d-flex justify-content-between mt-3'> 
                         <button class='btn btn-sm btn-primary' type='submit' onClick={buscarViaje}>Buscar</button>
                     </div>
                 </form>
             </div>
-
             <ViajesUsuarioComponent viajes={viajes}/>
         </div>    
-    
     </div>
 )
 }
